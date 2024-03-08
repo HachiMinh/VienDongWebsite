686e18e14c93fdd952e734e8a0ad456c39b238f1
HachiMinh <chichicvc1234@gmail.com>
2024-03-08 10:31:31 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,49 @@ export const metadata: Metadata = {
 
 export default function Home(): JSX.Element {
   return (
-    <div>
-      <h1>Home</h1>
+    <div id="home">
+      <div className="floating-text">
+        <h1>Công ty Viễn Đông</h1>
+        <h2>Placeholder h2 title...</h2>
+      </div>
+      <div className="floating-image">
+        <img alt="" src="/static/images/home_placeholder.png" />
+      </div>
+      <div className="grid-sections">
+        <div className="sections">
+          <div className="section-a">
+            <div className="upper">
+              <img alt="" src="/static/images/viendong_logo.jpg" />
+            </div>
+            <div className="lower">
+              <p>Du lịch</p>
+            </div>
+          <div className="section-b">
+            <div className="upper">
+              <img alt="" src="/static/images/viendong_logo.jpg" />
+            </div>
+            <div className="lower">
+              <p>Xây dựng</p>
+            </div>
+          </div>
+          <div className="section-c">
+            <div className="upper">
+              <img alt="" src="/static/images/viendong_logo.jpg" />
+            </div>
+            <div className="lower">
+              <p>Thương mại</p>
+            </div>
+          </div>
+          <div className="section-d">
+            <div className="upper">
+              <img alt="" src="/static/images/viendong_logo.jpg" />
+            </div>
+            <div className="lower">
+              <p>Section D</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
