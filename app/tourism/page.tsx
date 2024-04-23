9d5c2c4af0fc5b7a9a85b9b7a692d91ddd81d19b
Serious-senpai <57554044+Serious-senpai@users.noreply.github.com>
2024-04-23 21:27:26 +0700

    Add hyperlink to repository

--- app/tourism/page.tsx
+++ app/tourism/page.tsx
@@ -16,7 +16,7 @@ export default function Home(): React.JSX.Element {
           <h1>Khám phá thế giới cùng bạn!</h1>
 
           Công ty Viễn Đông tự hào là một trong những doanh nghiệp hàng đầu trong lĩnh vực du lịch tại Việt Nam.
-          Với hơn 1 năm hoạt động, chúng tôi đã và đang không ngừng nỗ lực mang đến cho khách hàng
+          Với nhiều năm hoạt động, chúng tôi đã và đang không ngừng nỗ lực mang đến cho khách hàng
           những dịch vụ du lịch chất lượng cao, đáp ứng mọi nhu cầu đa dạng của du khách.
         </div>
       </div>
